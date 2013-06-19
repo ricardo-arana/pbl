b0f161ab264630260b3f70d914a95df6433177df
Ricardo Gabriel <the.ricky.rish@gmail.com>
2013-06-19 03:27:09 -0500

    Create pbl.py

---
+++ pbl.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+'''
+##################################################################################
+#                          PHYTHON BACKLIGHT CONTROL                             #
+##################################################################################
+# Desarrollado por Ricardo Arana Reyes Guerrero                                  #
+# twitter @rickyrish                                                             #
+# Programa para gestionar el brillo de la pantalla de algunas portatiles como    #
+# Acer, Dell, entre otras por medio del comando xrandr                           #
+#                                                                                #
+##################################################################################
+'''
+
+import time, sys, subprocess,os
+import sys
+nivel = sys.argv[1]
+
+archi=open('/home/ricardo/Documentos/pbl/dato.txt','r')
+linea=archi.readline()
+linea=float(linea)
+archi.close()
+
+if linea>=0.4 and linea<=1:
+  
+	if nivel=="-a" and linea!=1:
+		archi=open('/home/ricardo/Documentos/pbl/dato.txt','w')
+		comando = "xrandr --output LVDS1 --brightness "+str(linea+0.1)
+		proceso = subprocess.Popen(comando , shell=True,stdin=subprocess.PIPE,stdout=subprocess.PIPE)
+		archi.write(str(linea+0.1))
+	elif nivel=="-b" and linea!=0.4:
+		archi=open('/home/ricardo/Documentos/pbl/dato.txt','w')
+		comando = "xrandr --output LVDS1 --brightness "+str(linea-0.1)
+		proceso = subprocess.Popen(comando , shell=True,stdin=subprocess.PIPE,stdout=subprocess.PIPE)
+		archi.write(str(linea-0.1))
+	archi.close()
+
