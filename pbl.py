0a9c5431c861186853cc8c03067ef717e21c486d
Ricardo Gabriel <the.ricky.rish@gmail.com>
2013-07-14 12:31:13 -0500

    Update pbl.py

--- pbl.py
+++ pbl.py
@@ -9,13 +9,18 @@
 # Acer, Dell, entre otras por medio del comando xrandr                           #
 #                                                                                #
 ##################################################################################
+Editar la ruta del archivo
+
 '''
+
+ruta='/home/ricardo/Documentos/pbl/dato.txt'
+
 
 import time, sys, subprocess,os
 import sys
 nivel = sys.argv[1]
 
-archi=open('/home/ricardo/Documentos/pbl/dato.txt','r')
+archi=open(ruta,'r')
 linea=archi.readline()
 linea=float(linea)
 archi.close()
@@ -23,12 +28,12 @@ archi.close()
 if linea>=0.4 and linea<=1:
   
 	if nivel=="-a" and linea!=1:
-		archi=open('/home/ricardo/Documentos/pbl/dato.txt','w')
+		archi=open(ruta,'w')
 		comando = "xrandr --output LVDS1 --brightness "+str(linea+0.1)
 		proceso = subprocess.Popen(comando , shell=True,stdin=subprocess.PIPE,stdout=subprocess.PIPE)
 		archi.write(str(linea+0.1))
 	elif nivel=="-b" and linea!=0.4:
-		archi=open('/home/ricardo/Documentos/pbl/dato.txt','w')
+		archi=open(ruta,'w')
 		comando = "xrandr --output LVDS1 --brightness "+str(linea-0.1)
 		proceso = subprocess.Popen(comando , shell=True,stdin=subprocess.PIPE,stdout=subprocess.PIPE)
 		archi.write(str(linea-0.1))
